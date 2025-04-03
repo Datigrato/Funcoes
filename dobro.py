cfddf08c792f33acfb5e107c4357db0831c267d7
Aristoteles Ferreira da Silva Filho - 1362413353 <1362413353@ulife.com.br>
2025-04-03 11:00:41 -0300

    funções python

---
+++ dobro.py
@@ -0,0 +1,7 @@
+numero = int (input('Digite um numero:'))
+
+def dobro(numero):
+    print(numero * 2)
+
+
+dobro(numero)
---
+++ parimpar.py
@@ -0,0 +1,10 @@
+numero = int (input('Digite um numero: '))
+
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        print('é par')
+    else:
+         print('é impar')
+               
+par_ou_impar(numero)
+
